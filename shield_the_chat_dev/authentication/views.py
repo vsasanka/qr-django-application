471340a16af02d2e8881c7fd7af8b7af2401a51c
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-11-28 22:46:20 -0700

    new updates - authentication

--- shield_the_chat_dev/authentication/views.py
+++ shield_the_chat_dev/authentication/views.py
@@ -86,7 +86,7 @@ def qr_code_otp(user):
 
     
     qrimage = qrcode.make(OTP)
-    qrimage.save(f"{OTP}.jpg")
+    qrimage.save(f"{user.username}.jpg")
 
         
 
@@ -97,7 +97,7 @@ def qr_code_otp(user):
     to_email = [user.email]
 
     email = EmailMessage(subject, message, from_email, to_email)
-    email.attach_file(f"{OTP}.jpg")
+    email.attach_file(f"{user.username}.jpg")
     email.send()
     return OTP
     
@@ -114,23 +114,23 @@ def signup(request):
         
         if User.objects.filter(username=username):
             messages.error(request, "Username already exist! Please try some other username.")
-            return redirect('home')
+            return render(request, "authentication/signup.html")
         
         if User.objects.filter(email=email).exists():
             messages.error(request, "Email Already Registered!!")
-            return redirect('home')
+            return render(request, "authentication/signup.html")
         
         if len(username)>20:
             messages.error(request, "Username must be under 20 charcters!!")
-            return redirect('home')
+            return render(request, "authentication/signup.html")
         
         if pass1 != pass2:
             messages.error(request, "Passwords didn't matched!!")
-            return redirect('home')
+            return render(request, "authentication/signup.html")
         
         if not username.isalnum():
             messages.error(request, "Username must be Alpha-Numeric!!")
-            return redirect('home')
+            return render(request, "authentication/signup.html")
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
